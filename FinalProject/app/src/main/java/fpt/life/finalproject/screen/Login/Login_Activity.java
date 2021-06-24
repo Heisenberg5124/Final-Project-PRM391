74f7844943f931f64b52d6a6212260f8d7d61b82
DHUNG\Hung Nguyen <duyhung542000@gmail.com>
2021-06-24 11:03:13 +0700

    Login_Feature_test

---
+++ FinalProject/app/src/main/java/fpt/life/finalproject/screen/Login/Login_Activity.java
@@ -0,0 +1,79 @@
+package fpt.life.finalproject.screen.Login;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import fpt.life.finalproject.R;
+
+public class Login_Activity extends AppCompatActivity {
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            this::onSignInResult
+    );
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        updateUI(mAuth.getCurrentUser());
+        createSignInIntent();
+    }
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            updateUI(user);
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            assert response != null;
+            Log.d("FailOnSignIn", "onSignInResult: "+ Objects.requireNonNull(response.getError()).getErrorCode());
+            updateUI(null);
+            // ...
+        }
+    }
+    public void createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build());
+
+        // Create and launch sign-in intent
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.logo)
+
+                .build();
+        signInLauncher.launch(signInIntent);
+        // [END auth_fui_create_intent]
+    }
+    private void updateUI(FirebaseUser user){
+        if(user!=null){
+            Log.d("logSuccessWithUser" , user.getUid());
+            startActivity(new Intent(Login_Activity.this,Profile_Activity.class));
+            finish();
+        }else {
+            Log.d("NoUser", "null");
+        }
+    }
+}
---
+++ FinalProject/app/src/main/java/fpt/life/finalproject/screen/Login/Profile_Activity.java
@@ -0,0 +1,52 @@
+package fpt.life.finalproject.screen.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import fpt.life.finalproject.R;
+
+public class Profile_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        TextView username = findViewById(R.id.username);
+        TextView phone = findViewById(R.id.phone);
+        TextView email = findViewById(R.id.email);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser()!=null){
+            if(auth.getCurrentUser().getPhoneNumber()!=null){
+                phone.setText("Phone: "+auth.getCurrentUser().getPhoneNumber());
+            }
+            if(auth.getCurrentUser().getDisplayName()!=null){
+                username.setText("Username: "+auth.getCurrentUser().getDisplayName());
+            }
+            if(auth.getCurrentUser().getEmail()!=null){
+                email.setText("Email: "+auth.getCurrentUser().getEmail());
+            }
+        }
+        Button logout = findViewById(R.id.logout_btn);
+        logout.setOnClickListener(v -> signOut());
+    }
+    public void signOut() {
+        // [START auth_fui_signout]
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(task -> {
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(Profile_Activity.this,Login_Activity.class));
+                    finish();
+                });
+        // [END auth_fui_signout]
+    }
+}
